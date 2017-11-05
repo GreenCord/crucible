fea5a2e7f9ba3a3d7aba8ee67e5e4b914ff766dc
GreenCord <steve@nevizen.com>
2017-11-05 10:35:47 -0500

    Updated pregame phase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,166 @@ $(document).ready(function(){
 			// attack power
 			// current attack power
 			// counter attack power
-		
+		chars: ['char1','char2','char3','char4'],
+		initpanels: ['#char-select','#char-chosen','#char-attackpool','#fight-section','#game-over'],
+
+		char1: {
+			id: 'char1',
+			name: 'Character 1',
+			image: '<img src="https://placehold.it/320x240" alt="Character 1" />',
+			basehp: 100,
+			currhp: null,
+			baseatk: 5,
+			curratk: null,
+			cntratk: 10
+		},
+
+		char2: {
+			id: 'char2',
+			name: 'Character 2',
+			image: '<img src="https://placehold.it/320x240" alt="Character 2" />',
+			basehp: 100,
+			currhp: null,
+			baseatk: 5,
+			curratk: null,
+			cntratk: 10
+		},
+
+		char3: {
+			id: 'char3',
+			name: 'Character 3',
+			image: '<img src="https://placehold.it/320x240" alt="Character 3" />',
+			basehp: 100,
+			currhp: null,
+			baseatk: 5,
+			curratk: null,
+			cntratk: 10
+		},
+
+		char4: {
+			id: 'char4',
+			name: 'Character 4',
+			image: '<img src="https://placehold.it/320x240" alt="Character 4" />',
+			basehp: 100,
+			currhp: null,
+			baseatk: 5,
+			curratk: null,
+			cntratk: 10
+		},
+
+		player: {},
 
 		//define game phase tracker
 
+		phase: 'pregame', // possible phases: pregame, select, start, begin, battle, gameover
+
 		//define game initialization
 
+		resetChar: function(objid) {
+			console.log('Resetting: ' + objid);
+			var character = this[objid];
+			character.currhp = character.basehp;
+			character.curratk = character.baseatk;
+		},
+
+		toggleArea: function(areaid) {
+			// if ($(areaid).is(':hidden')) {
+			// 	$(areaid).show();
+			// } else {
+			// 	$(areaid).hide();
+			// }
+			$(areaid).slideToggle();
+		},
+
+		initializeGame: function(){
+			var charkeys = this.chars;
+			var areakeys = this.initpanels;
+			var gameobj = this;
+			console.log('Char Array: ' + charkeys);
+			$.each(charkeys, function(index, value){ // reset hp and attack power
+				console.log(index + ': ' + value);
+				gameobj.resetChar(value);
+			});
+		},
+
+		toggleAllPanels: function(){
+
+			// $.each(areakeys, function(index, value){ // hide or show game areas
+			// 	gameobj.toggleArea(value);	
+			// });			
+		},
+
+		createTile: function(objid,targetid){
+			/* Tile structure
+			<div id="" class="character-tile clickable">
+                <div class="character-name">Character 1</div>
+                <div class="character-pic"><img src="https://placehold.it/320x240" alt="char pic" /></div>
+                <div class="character-hp">100</div>
+            </div> 
+            */
+            var gameobjid = this[objid];
+            console.log('Creating tiles');
+            console.log('objid = ' + objid);
+            console.log('targetid = ' + targetid);
+            // start building tile
+            var createid = 'choose-' + gameobjid.id;
+            $(targetid).append('<div id="' + createid + '"></div>');
+            var newdiv = $('#'+createid);
+            newdiv.addClass('character-tile clickable');
+            newdiv.append('<div class="character-name">' + gameobjid.name + '</div>');
+            newdiv.append('<div class="character-pic" alt="'+ gameobjid.name + '">' + gameobjid.image + '</div>');
+            newdiv.append('<div class="character-hp">' + gameobjid.currhp + '</div>');
+
+		},
+
+		clickHandler: function(objid){
 		//define click action functions
+			console.log('clickHandler called:');
+			console.log(this);
+			var charkeys = this.chars;
+			var areakeys = this.initpanels;
+			var gameobj = this;
+			switch (this.phase) {
+				// game phase pregame
+				case 'pregame': 
+
+					switch (objid) { // check object clicked
+						
+						case '#replay-btn':  // should always be this
+							
+							// populate #char-select-pool
+							$.each(charkeys, function(index, value){
+								gameobj.createTile(value,'#char-select-pool');
+							});
+							
+							// show char select and update game phase
+							gameobj.phase = 'select';
+							gameobj.toggleArea('#char-select');
+							gameobj.toggleArea('#game-over');
+						break;
+
+						default:
+							alert('Something went wrong. (Phase - Pregame, clicked item ' + objid + ')');
+
+					} // end pregame switch
+					break;
+
+				//game phase select - select character to play
+				case 'select':
+
+					// put clicked character into player object
+
+					// put player character into #char-chosen-pool
+					// remove clickable class
+
+					// put other characters into #char-attack-pool
+
+					// show #char-chosen and #char-attackpool, hide #char-select
+					break;
+
+
+	
+	
 
 			// game phase start
 			// choose character
@@ -30,11 +183,22 @@ $(document).ready(function(){
 
 			// game phase battle over
 			// update game win/loss and display game over section
+			} // end game phase switch
+		}
 
 	};// end game object
 
+	
 	// initialize game
+	game.initializeGame();
+	console.log('Initialized Game Object');
+	console.log(game);
 
 	// capture click action
+
+	$('.clickable').on('click', function(){
+		var thisid = '#' + this.id;
+		game.clickHandler(thisid);		
+	});
 
 });
