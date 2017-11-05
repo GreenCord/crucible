4e171c20367db8a27fca6e58c13b46848496c720
GreenCord <steve@nevizen.com>
2017-11-05 08:42:37 -0500

    Framed out HTML, pseudocoded game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	// define game object
+	var game = {
+
+		//define characters
+			// name
+			// image
+			// hit points
+			// current hit points
+			// attack power
+			// current attack power
+			// counter attack power
+		
+
+		//define game phase tracker
+
+		//define game initialization
+
+		//define click action functions
+
+			// game phase start
+			// choose character
+
+			// game phase battle begin
+			// choose defender to attack
+
+			// game phase battle in progress
+			// click attack button
+
+			// game phase battle over
+			// update game win/loss and display game over section
+
+	};// end game object
+
+	// initialize game
+
+	// capture click action
+
+});
